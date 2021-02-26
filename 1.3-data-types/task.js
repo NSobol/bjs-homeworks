1eec079872334ad5da4f74a2da96959456cb112a
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-02-26 21:22:47 +0300

    Внесены исправления в код

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,14 +1,27 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
+    percent /= 100;
+    let P = (percent / 12);
+    let Amount = parseInt(amount, 10);
+    let Contribution = parseInt(contribution, 10);
+    let S = Amount - Contribution;
+    let times = new Date;
+    let day = (date.getTime() - times.getTime()) / (1000 * 3600 * 24);
+    let month = 30.4375;
+    let n = Math.round(day / month);
+    let payment = S * (P + P / (((1 + P) ** n) - 1));
+    let totalAmount = parseFloat((payment * n).toFixed(2));
 
-
-
-    // return totalAmount;
+    return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-
-
-    // return greeting;
+    if (name == "" || typeof (name) == "undefined") {
+        return `Привет, мир! Меня зовут Аноним.`;
+    } else {
+        return `Привет, мир! Меня зовут ${name}.`
+    }
 }
