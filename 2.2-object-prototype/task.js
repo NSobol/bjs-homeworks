1db82942bd4150da0c65cd1ed28ce859fed16d97
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-03-06 22:03:26 +0300

    изменено

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -2,10 +2,14 @@
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
+
+
     // return averageMark
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
+
+
     // return verdict
 }
