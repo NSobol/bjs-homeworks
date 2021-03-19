3c280ebffb504a0fe0c6cc63c28bfa6269384534
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-03-19 09:37:01 +0300

    Внесены исправления в первую задачу

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -9,28 +9,23 @@ function getNames() {
 //getNames();
 
 function getCountReliableWeapons(param) {
-    const countReliable = (weapons.map(item => item.durability)).filter(durability => durability > param);
-    return countReliable.length;
+    return weapons.filter(item => item.durability > param).length;
 }
 
 //getCountReliableWeapons(200);
 
 function hasReliableWeapons(param) {
-    return (weapons.map(item => item.durability)).some(elem => elem > param);
+    return weapons.map(item => item.durability).some(elem => elem > param);
 }
 
 //hasReliableWeapons(400);
 
 function getReliableWeaponsNames(param) {
-    return (weapons.filter(item => item.durability > param)).map(function (item) {
-        return item.name
-    });
+    return (weapons.filter(item => item.durability > param)).map(item => item.name);
 }
 
 //getReliableWeaponsNames(280);
 
 function getTotalDamage() {
-    return (weapons.map(item => item.attack)).reduce(function (a, b) {
-        return a + b;
-    });
+    return weapons.reduce((prev, cur) => prev + cur.attack, 0);
 }
